357f3647a3c757222a3d5d0021354862ebb115b2
khalatevarun <khalatevarun@gmail.com>
2025-09-03 08:54:30 -0500

    add boilerplate code

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors())
+app.use(express.json());
+
+app.listen(3000);
+
+app.post("/deploy",(req,res) => {
+    const repoUrl = req.body.repoUrl;
+    console.log(repoUrl);
+
+    res.json({})
+})
+app.listen(3000);
