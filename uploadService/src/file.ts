92318aa2e7c5dc4e3d103c957d7bc6532fd59b8c
khalatevarun <khalatevarun@gmail.com>
2025-09-04 09:04:35 -0500

    move the upload service codein a dedicated directory

---
+++ uploadService/src/file.ts
@@ -0,0 +1,17 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+export function getAllFiles(dir: string): string[] {
+    let results: string[] = [];
+    const list = fs.readdirSync(dir);
+    list.forEach((file) => {
+        const filePath = path.join(dir, file);
+        const stat = fs.statSync(filePath);
+        if (stat && stat.isDirectory()) {
+            results = results.concat(getAllFiles(filePath));
+        } else {
+            results.push(filePath);
+        }
+    });
+    return results;
+}
